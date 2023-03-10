e3a4d3e5b881769ff08c7ab04448f40ece9d5063
TrishlaBhakat <36823243+TrishlaBhakat@users.noreply.github.com>
2023-03-10 14:31:36 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    reactStrictMode: true,
+    publicRuntimeConfig: {
+        apiUrl: process.env.NODE_ENV === 'development'
+            ? 'https://s32k2.sse.codesandbox.io/api' // development api
+            : 'https://s32k2.sse.codesandbox.io/api' // production api
+    }
+}
